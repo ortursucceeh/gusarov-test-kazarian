ebc1187ddf834013f2b90f2f809131109a69ec86
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2024-09-24 11:14:42 +0300

    add input annotation

---
+++ src/components/InputAnnotation/InputAnnotation.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import { SIZES } from "../../utils/constants";
+import styles from "./InputAnnotation.module.css";
+
+type InputAnnotationProps = {
+  size?: SIZES;
+  disabled?: boolean;
+  isError?: boolean;
+  text: string;
+};
+
+export const InputAnnotation = (props: InputAnnotationProps) => {
+  const { text, size, isError, disabled } = props;
+
+  return (
+    <span
+      className={clsx(styles.inputAnnotation, {
+        [styles.L]: [SIZES.L, SIZES.XL].includes(size!),
+        [styles.error]: isError,
+        [styles.disabled]: disabled,
+      })}
+    >
+      {text}
+    </span>
+  );
+};
