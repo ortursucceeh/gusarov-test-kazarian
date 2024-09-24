a9b346340fee71644f2982d212d383144f6da84b
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2024-09-24 11:14:12 +0300

    add form

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,81 @@
+import styles from "./Form.module.css";
+
+import { Button } from "../Button";
+import { InputGroup } from "../InputGroup/InputGroup";
+
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
+import type { FormOptions } from "../../utils/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignInSchema, SignInSchemaType } from "../../utils/schemas";
+import { SIZES } from "../../utils/constants";
+import { CircleHelp, KeyRound, Mail } from "lucide-react";
+
+export const Form = () => {
+  const { handleSubmit, control, formState } = useForm<SignInSchemaType>({
+    resolver: zodResolver(SignInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { errors } = formState;
+
+  const submit: SubmitHandler<FormOptions> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form className={styles.Form} onSubmit={handleSubmit(submit)}>
+      <div className={styles.header}>
+        <h2>Login</h2>
+        <p>Please enter your credentials to login</p>
+      </div>
+
+      <Controller
+        name="email"
+        control={control}
+        render={({ field: { value, onChange } }) => (
+          <InputGroup
+            id="email"
+            label="Email"
+            withHint
+            required
+            size={SIZES.L}
+            inputType="email"
+            placeholder="Email"
+            iconLeft={<Mail size={16} />}
+            iconRight={<CircleHelp size={16} />}
+            value={value}
+            onChange={onChange}
+            error={errors.email}
+            text={errors.email ? errors.email.message! : "This is a hint to help user."}
+          />
+        )}
+      />
+      <Controller
+        name="password"
+        control={control}
+        render={({ field: { value, onChange } }) => (
+          <InputGroup
+            id="password"
+            label="Password"
+            withHint
+            required
+            size={SIZES.L}
+            inputType="password"
+            placeholder="Password"
+            iconRight={<KeyRound size={16} />}
+            value={value}
+            onChange={onChange}
+            error={errors.password}
+            text={errors.password ? errors.password.message! : "This is a hint to help user."}
+          />
+        )}
+      />
+
+      <Button>Sign in</Button>
+    </form>
+  );
+};
